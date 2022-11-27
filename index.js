e1dd36183413c77bccc800e714fa428fe0c6a520
Alex <alexmwangithuku001@gmail.com>
2022-11-27 11:59:42 +0300

    Done with assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = function(x=2000, y=100){
+   return x/y
+}
+// console.log(divide(2000, 100))
+
+const square = x => x**2
+// console.log(square(2))
+
+const add = (a, b) => (a + b)
+  add(3, 4)
+  
